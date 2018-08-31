f121f3ce1cf143550b2d423e790b962c9c6d01a9
Raymond Duckworth <raymondduckworth@nova-mac.nova2k3.local>
2018-08-31 16:15:40 -0700

    updated index page response

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express"),
 	 		app = express();
 
 app.get("/", function(req, res) {
-	res.send("Hello world");
+	res.send("This is the index page");
 });
 
 app.listen(3000, function(){
