4bb55e7ef40889d6564f6b44d800e9d479e5ef32
Raymond Duckworth <raymondxduckworth@gmail.com>
2018-09-04 20:00:25 -0700

    Update server file and add views

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express"),
+			bodyParser = require("body-parser"),
 	 		app = express();
 
 app.get("/", function(req, res) {
-	res.send("This is the index page");
+	res.render("index");
 });
 
-app.listen(3000, function(){
+app.get("/contacts", function(req, res) {
+	res.send("This is the contacts page.");
+});
+
+app.post("/contacts/new", function(req, res) {
+	res.send("This is the post route.");
+});
+
+app.listen(8000, function(){
 	console.log("Server is running");
 });
